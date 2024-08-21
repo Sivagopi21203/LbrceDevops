d397163cce48cc6d55abc3813914d9f48ca5c78d
AdmiN <AdmiN@ITWS0023>
2024-08-21 15:00:34 +0530

    commit

---
+++ src/program1/login.java
@@ -0,0 +1,11 @@
+package program1;
+
+public class login {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to CICD pipeline");
+		// TODO Auto-generated method stub
+
+	}
+
+}
