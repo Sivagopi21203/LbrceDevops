37ea4f20d2571d56e44c3f8f889bdf8dcbed2c22
AdmiN <AdmiN@ITWS0023>
2024-08-21 15:04:16 +0530

    commit

--- src/program1/login.java
+++ src/program1/login.java
@@ -3,7 +3,7 @@ package program1;
 public class login {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to CICD pipeline");
+		System.out.println("login to CICD pipeline");
 		// TODO Auto-generated method stub
 
 	}
